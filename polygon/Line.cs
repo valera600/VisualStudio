ee3765177f462273f2ae244590d1118af853f4da
valera600 <valera.naydenko@gmail.com>
2015-10-08 21:42:30 +0300

    Добавлены классы линия и полигон

---
+++ polygon/Line.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace polygon
+{
+    class Line
+    {
+        public Point a { get; set; }
+        public Point b { get; set; }
+
+        public Line()
+        {
+            a = new Point();
+            b = new Point();
+        }
+
+        public Line(Point _a, Point _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        /// <summary>
+        /// Метод находящий ориентированную площадь треугольника заданного тремя точками
+        /// </summary>
+        /// <param name="a">Первая точка треугольника</param>
+        /// <param name="b">Вторая точка треугольника</param>
+        /// <param name="c">Третья точка треугольника</param>
+        /// <returns>Целочесленная площадь треугольника</returns>
+        int area (Point a, Point b, Point c) {
+	        return (b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X);
+        }
+
+        /// <summary>
+        /// Функция обмена значений двух переменных одного типа
+        /// </summary>
+        /// <typeparam name="T">Тип переменных, значения которых обмениваются</typeparam>
+        /// <param name="lhs">Первая переменная</param>
+        /// <param name="rhs">Вторая переменная</param>
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        bool intersect_1 (int a, int b, int c, int d) {
+	        if (a > b)  Swap (ref a, ref b);
+	        if (c > d)  Swap (ref c, ref d);
+	      return (Math.Max(a,c) < Math.Min(b,d)) || (a==b && b==c && c==d);
+        }
+
+        public bool intresect(Line line)
+        {
+            return intersect(line.a, line.b);
+        }
+
+        public bool intersect (Point c, Point d) {
+	        return intersect_1 (a.X, b.X, c.X, d.X)
+		&& intersect_1 (a.Y, b.Y, c.Y, d.Y)
+		&& area(a,b,c) * area(a,b,d) <= 0
+		&& area(c,d,a) * area(c,d,b) <= 0;
+        }
+    }
+}
---
+++ polygon/Polygon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace polygon
+{
+    /// <summary>
+    /// Данный класс представляет полигон, который описан коллекцией точек
+    /// </summary>
+    class Polygon
+    {
+        public Collection<Point> pointCollection { get; set; }
+
+        public Polygon() 
+        {
+            pointCollection = new Collection<Point>();
+        }
+
+        public Polygon(Collection<Point> _pointCollection)
+        {
+            pointCollection = _pointCollection;
+        }
+
+        /// <summary>Данный метод добавляет точку к полигону</summary>
+        /// <param name="_point">Точка, которая добавляется к полигону</param>
+        public void addPoint(Point _point)
+        {
+            pointCollection.Add(_point);
+        }
+    }
+}
