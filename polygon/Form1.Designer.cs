60737dbfca9816444876d3ee019575c977ec07a7
valera600 <valera.naydenko@gmail.com>
2015-10-08 21:39:59 +0300

    Рисует один полигон, пробный алгоритм проверки на самопересечение (не правильно работает, когда ставишь следующую точку правее, когда до этого было сверху вниз поставленна точки)

--- polygon/Form1.Designer.cs
+++ polygon/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.workSpace = new System.Windows.Forms.PictureBox();
+            this.btStartPolygonA = new System.Windows.Forms.Button();
+            this.btStopPolygonA = new System.Windows.Forms.Button();
+            this.tbPointsPolygonA = new System.Windows.Forms.TextBox();
+            this.cbFillPolygon = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.workSpace)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,28 +40,82 @@
             // 
             this.workSpace.BackColor = System.Drawing.Color.White;
             this.workSpace.Cursor = System.Windows.Forms.Cursors.Cross;
-            this.workSpace.Location = new System.Drawing.Point(102, 35);
+            this.workSpace.Enabled = false;
+            this.workSpace.Location = new System.Drawing.Point(130, 86);
             this.workSpace.Name = "workSpace";
-            this.workSpace.Size = new System.Drawing.Size(440, 303);
+            this.workSpace.Size = new System.Drawing.Size(429, 370);
             this.workSpace.TabIndex = 0;
             this.workSpace.TabStop = false;
+            this.workSpace.MouseUp += new System.Windows.Forms.MouseEventHandler(this.workSpace_MouseUp);
+            // 
+            // btStartPolygonA
+            // 
+            this.btStartPolygonA.Location = new System.Drawing.Point(12, 12);
+            this.btStartPolygonA.Name = "btStartPolygonA";
+            this.btStartPolygonA.Size = new System.Drawing.Size(222, 31);
+            this.btStartPolygonA.TabIndex = 1;
+            this.btStartPolygonA.Text = "Начать рисовать первый полигон";
+            this.btStartPolygonA.UseVisualStyleBackColor = true;
+            this.btStartPolygonA.Click += new System.EventHandler(this.btStartPolygonA_Click);
+            // 
+            // btStopPolygonA
+            // 
+            this.btStopPolygonA.Enabled = false;
+            this.btStopPolygonA.Location = new System.Drawing.Point(12, 49);
+            this.btStopPolygonA.Name = "btStopPolygonA";
+            this.btStopPolygonA.Size = new System.Drawing.Size(222, 31);
+            this.btStopPolygonA.TabIndex = 2;
+            this.btStopPolygonA.Text = "Закончить рисование первого полигона";
+            this.btStopPolygonA.UseVisualStyleBackColor = true;
+            this.btStopPolygonA.Click += new System.EventHandler(this.btStopPolygonA_Click);
+            // 
+            // tbPointsPolygonA
+            // 
+            this.tbPointsPolygonA.Location = new System.Drawing.Point(18, 93);
+            this.tbPointsPolygonA.Multiline = true;
+            this.tbPointsPolygonA.Name = "tbPointsPolygonA";
+            this.tbPointsPolygonA.ReadOnly = true;
+            this.tbPointsPolygonA.Size = new System.Drawing.Size(106, 363);
+            this.tbPointsPolygonA.TabIndex = 3;
+            this.tbPointsPolygonA.TabStop = false;
+            this.tbPointsPolygonA.Text = "Точки полигона А:";
+            // 
+            // cbFillPolygon
+            // 
+            this.cbFillPolygon.AutoSize = true;
+            this.cbFillPolygon.Location = new System.Drawing.Point(240, 37);
+            this.cbFillPolygon.Name = "cbFillPolygon";
+            this.cbFillPolygon.Size = new System.Drawing.Size(146, 17);
+            this.cbFillPolygon.TabIndex = 4;
+            this.cbFillPolygon.Text = "Закрашивать полигоны";
+            this.cbFillPolygon.UseVisualStyleBackColor = true;
+            this.cbFillPolygon.CheckedChanged += new System.EventHandler(this.cbFillPolygon_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 404);
+            this.ClientSize = new System.Drawing.Size(664, 468);
+            this.Controls.Add(this.cbFillPolygon);
+            this.Controls.Add(this.tbPointsPolygonA);
+            this.Controls.Add(this.btStopPolygonA);
+            this.Controls.Add(this.btStartPolygonA);
             this.Controls.Add(this.workSpace);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Polygon";
             ((System.ComponentModel.ISupportInitialize)(this.workSpace)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox workSpace;
+        private System.Windows.Forms.Button btStartPolygonA;
+        private System.Windows.Forms.Button btStopPolygonA;
+        private System.Windows.Forms.TextBox tbPointsPolygonA;
+        private System.Windows.Forms.CheckBox cbFillPolygon;
     }
 }
 
--- polygon/Form1.cs
+++ polygon/Form1.cs
@@ -12,9 +12,84 @@ namespace polygon
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Polygon a, b;
+        Byte currentPolygon = 0;    //хранит номер редактируемого полигона в данный момент {0 - ни один, 1 - первый, 2 - второй}
         public Form1()
         {
             InitializeComponent();
+            g = workSpace.CreateGraphics();
+            a = new Polygon();
+            b = new Polygon();
         }
+
+        private void btStartPolygonA_Click(object sender, EventArgs e)
+        {
+            btStartPolygonA.Enabled = false;    //отключаем кнопку запуска рисования полигона А
+            btStopPolygonA.Enabled = true;  //включаем кнопку остановки рисования полигона А
+            workSpace.Enabled = true;   //включаем область для рисования
+            currentPolygon = 1; //на данный момент рисуется первый полигон
+        }
+
+        private void workSpace_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point newPoint = new Point(e.X, e.Y);  //точка, которую добавил пользователь
+            switch (currentPolygon)
+            {
+                case 1:
+                    int countPoints = a.pointCollection.Count;
+                    Boolean badPoint = false;
+                    if(countPoints >= 2)
+                    {
+                        Point[] points = a.pointCollection.ToArray();
+                        Line newLine = new Line(points[countPoints-1], newPoint);
+                        for(int i = 0; i < a.pointCollection.Count-1; i++)
+                        {
+                            if(newLine.intersect(points[i], points[i+1]))
+                            {
+                                badPoint = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!badPoint)
+                    {
+                        a.addPoint(newPoint);
+                        tbPointsPolygonA.Text += (Environment.NewLine + newPoint.ToString());
+                        g.FillRectangle(Brushes.Black, newPoint.X, newPoint.Y, 1, 1);   //рисуем выбранную пользователем точку
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Данная точка даёт самопересечение!");
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        private void btStopPolygonA_Click(object sender, EventArgs e)
+        {
+            btStartPolygonA.Enabled = true;    //включаем кнопку запуска рисования полигона А
+            btStopPolygonA.Enabled = false;  //выключаем кнопку остановки рисования полигона А
+            workSpace.Enabled = false;   //выключаем область для рисования
+            currentPolygon = 0; //на данный момент ни какой полигон не рисуется
+            g.DrawPolygon(Pens.Red, a.pointCollection.ToArray());
+        }
+
+        private void cbFillPolygon_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbFillPolygon.Checked)
+                g.FillPolygon(Brushes.Red, a.pointCollection.ToArray());
+            else
+            {
+                g.FillPolygon(Brushes.White, a.pointCollection.ToArray());
+                g.DrawPolygon(Pens.Red, a.pointCollection.ToArray());
+            }
+        }
+
     }
 }
