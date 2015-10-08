85b470fbdcb02fe1dea266c47a11bdd4417de016
valera600 <valera.naydenko@gmail.com>
2015-10-08 13:59:05 +0300

    start project

---
+++ polygon/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace polygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.workSpace = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // workSpace
+            // 
+            this.workSpace.BackColor = System.Drawing.Color.White;
+            this.workSpace.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.workSpace.Location = new System.Drawing.Point(102, 35);
+            this.workSpace.Name = "workSpace";
+            this.workSpace.Size = new System.Drawing.Size(440, 303);
+            this.workSpace.TabIndex = 0;
+            this.workSpace.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 404);
+            this.Controls.Add(this.workSpace);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.workSpace)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox workSpace;
+    }
+}
+
